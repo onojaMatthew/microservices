7594937f70d3bdfc62efc8afb412c3778cea2f6b
Onoja <onojamatthew59@gmail.com>
2019-10-18 03:24:18 +0100

    added action and reducer for poll

---
+++ client/src/store/actions/actions_polls.js
@@ -0,0 +1,390 @@
+import { userType } from "../../helpers/authenticate";
+/**
+ * Create action type
+ */
+export const CREATE_POLL_START = "CREATE_POLL_START";
+export const CREATE_POLL_SUCCESS = "CREATE_POLL_SUCCRSS";
+export const CREATE_POLL_FAILED = "CREATE_POLL_FAILED";
+
+/**
+ * Get action type
+ */
+export GET_POLL_START = "GET_POLL_START";
+export GET_POLL_SUCCESS = "GET_POLL_SUCCESS";
+export GET_POLL_FAILED = "GET_POLL_FAILED";
+
+/**
+ * tag action type
+ */
+export const TAG_POLL_START = "TAG_POLL_START";
+export const TAG_POLL_SUCCESS = "TAG_POLL_SUCCESS";
+export const TAG_POLL_FAILED = "TAG_POLL_FAILED";
+
+/**
+ * Like action type
+ */
+export const LIKE_POLL_START = "LIKE_POLL_START";
+export const LIKE_POLL_SUCCESS = "LIKE_POLL_SUCCESS";
+export const LIKE_POLL_FAILED = "LIKE_POLL_FAILED";
+
+/**
+ * Vote action type
+ */
+export const VOTE_POLL_START = "VOTE_POLL_START";
+export const VOTE_POLL_SUCCESS = "VOTE_POLL_SUCCESS";
+export const VOTE_POLL_FAILED = "VOTE_POLL_FAILED";
+
+/**
+ * Disable ation type
+ */
+export const DISABLE_POLL_START = "DISABLE_POLL_START";
+export const DISABLE_POLL_SUCCES = "DISABLE_POLL_SUCCESS";
+export const DISABLE_POLL_FAILED = "DISABLE_POLL_FAILED";
+
+/**
+ * Upload action type
+ */
+export const UPLOAD_POLL_PHOTO_START = "UPLOAD_POLL_PHOTO_START";
+export const UPLOAD_POLL_PHOTO_SUCCESS = "UPLOAD_POLL_PHOTO_SUCCESS";
+export const UPLOAD_POLL_PHOTO_FAILED = "UPLOAD_POLL_PHOTO_FAILED";
+
+/**
+ * Delete action type
+ */
+export const DELETE_POLL_START = "DELETE_POLL_START";
+export const DELETE_POLL_SUCCESS = "DELETE_POLL_SUCCESS";
+export const DELETE_POLL_FAILED = "DELETE_POLL_FAILED";
+
+const BASE_URL = "http://localhost:3020/api/v1/user";
+const MAIN_BASE_URL = "http://localhost:3030/api/v1/poll";
+
+export const createPollStart = () => {
+  return {
+    type: CREATE_POLL_START
+  }
+}
+
+export const createPollSuccess = ( data ) => {
+  return {
+    type: CREATE_POLL_SUCCESS,
+    data
+  }
+}
+
+export const createPollFailed = ( error ) => {
+  return {
+    type: CREATE_POLL_FAILED,
+    error
+  }
+}
+
+
+export const createPoll = (data, userId) => {
+  return dispatch => {
+    dispatch( createPollStart() );
+    fetch( `${ BASE_URL }/create/${ userType() }/${ userId }`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ACCEPT: "application/json"
+      },
+      body: JSON.stringify( data )
+    } )
+      .then( response => response.json() )
+      .then( data => {
+        dispatch( createPollSuccess( data ) );
+      } )
+      .catch( err => {
+        dispatch( createPollFailed( err.message ) );
+      } );
+  }
+}
+// tTy
+
+export const getPollStart = () => {
+  return {
+    type: GET_POLL_START
+  }
+}
+
+export const getPollSuccess = (data) => {
+  return {
+    type: GET_POLL_SUCCESS,
+    data
+  }
+}
+
+export const getPollFailed = (error) => {
+  return {
+    type: GET_POLL_FAILED,
+    error
+  }
+}
+
+export const getPoll = () => {
+  return dispatch => {
+    dispatch(getPollStart())
+    fetch( `${ MAIN_BASE_URL }/all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ACCEPT: "application/json"
+      }
+    } )
+      .then( response => response.json() )
+      .then( resp => {
+        dispatch( getPollSuccess( resp ) );
+      } )
+      .catch( err => {
+        dispatch( getPollFailed( err.message );)
+      });
+  }
+}
+
+
+export const tagPollStart = () => {
+  return {
+    type: TAG_POLL_START
+  }
+}
+
+export const tagPollSuccess = (data) => {
+  return {
+    type: TAG_POLL_SUCCES,
+    data
+  }
+}
+
+export const tagPollFailed = (error) => {
+  return {
+    type: TAG_POLL_FAILED,
+    error
+  }
+
+
+export const tagPoll = (data) => {
+  return dispatch => {
+    dispatch( tagPollStart() );
+    fetch( `${ BASE_URL }/tags/${ userType() }/${ pollId }`, {
+      method: "PUT",
+      headers: {
+        ACCEPT: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    } )
+      .then( response => response.json() )
+      .then( resp => {
+        dispatch( tagPollSuccess( resp ) );
+      } )
+      .catch( err => {
+        dispatch( tagPollFailed( err.message );)
+      });
+  }
+}
+
+
+export const likePollStart = () => {
+  return {
+    type: LIKE_POLL_START
+  }
+}
+
+export const likePollSuccess = ( data ) => {
+  return {
+    type: LIKE_POLL_SUCCES,
+    data
+  }
+}
+
+export const likePollFailed = ( error ) => {
+  return {
+    type: LIKE_POLL_FAILED,
+    error
+  }
+
+
+  export const likePoll = ( data,pollId, userId,  ) => {
+    return dispatch => {
+      dispatch( likePollStart() );
+      fetch( `${ BASE_URL }/like/${ userType() }/${ pollId }/${userId}`, {
+        method: "PUT",
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( data )
+      } )
+        .then( response => response.json() )
+        .then( resp => {
+          dispatch( likePollSuccess( resp ) );
+        } )
+        .catch( err => {
+          dispatch( likePollFailed( err.message );)
+    });
+  }
+}
+
+
+export const votePollStart = () => {
+  return {
+    type: VOTE_POLL_START
+  }
+}
+
+export const votePollSuccess = ( data ) => {
+  return {
+    type: VOTE_POLL_SUCCES,
+    data
+  }
+}
+
+export const votePollFailed = ( error ) => {
+  return {
+    type: VOTE_POLL_FAILED,
+    error
+  }
+
+
+  export const votePoll = ( data, pollId, userId, ) => {
+    return dispatch => {
+      dispatch( votePollStart() );
+      fetch( `${ BASE_URL }/vote/${ userType() }/${ pollId }/${ userId }`, {
+        method: "PUT",
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( data )
+      } )
+        .then( response => response.json() )
+        .then( resp => {
+          dispatch( votePollSuccess( resp ) );
+        } )
+        .catch( err => {
+          dispatch( votePollFailed( err.message );)
+    });
+  }
+}
+
+
+export const disablePollStart = () => {
+  return {
+    type: DISABLE_START
+  }
+}
+
+export const disablePollSuccess = ( data ) => {
+  return {
+    type: DISABLE_POLL_SUCCES,
+    data
+  }
+}
+
+export const disablePollFailed = ( error ) => {
+  return {
+    type: DISABLE_POLL_FAILED,
+    error
+  }
+
+
+  export const disablePoll = ( data, pollId, userId, ) => {
+    return dispatch => {
+      dispatch( disablePollStart() );
+      fetch( `${ BASE_URL }/disable/${ userType() }/${ pollId }/${ userId }`, {
+        method: "PUT",
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( data )
+      } )
+        .then( response => response.json() )
+        .then( resp => {
+          dispatch( disablePollSuccess( resp ) );
+        } )
+        .catch( err => {
+          dispatch( disablePollFailed( err.message );)
+    });
+  }
+}
+
+
+export const deletePollStart = () => {
+  return {
+    type: DISABLE_START
+  }
+}
+
+export const deletePollSuccess = ( data ) => {
+  return {
+    type: DISABLE_POLL_SUCCES,
+    data
+  }
+}
+
+export const deletePollFailed = ( error ) => {
+  return {
+    type: DISABLE_POLL_FAILED,
+    error
+  }
+
+
+  export const deletePoll = ( data, pollId, userId, ) => {
+    return dispatch => {
+      dispatch( deletePollStart() );
+      fetch( `${ MAIN_BASE_URL }/delete/${ userType() }/${ pollId }`, {
+        method: "PUT",
+        headers: {
+          ACCEPT: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( data )
+      } )
+        .then( response => response.json() )
+        .then( resp => {
+          dispatch( deletePollSuccess( resp ) );
+        } )
+        .catch( err => {
+          dispatch( deletePollFailed( err.message );)
+    });
+  }
+}
+
+
+export const uploadPollStart = () => {
+  return {
+    type: UPLOAD_POLL_START
+  }
+}
+
+export const uploadPollSuccess = ( data ) => {
+  return {
+    type: UPLOAD_POLL_SUCCES,
+    data
+  }
+}
+
+export const uploadPollFailed = ( error ) => {
+  return {
+    type: UPLOAD_POLL_FAILED,
+    error
+  }
+
+
+  export const uploadPoll = ( data, pollId, userId, ) => {
+    return dispatch => {
+      dispatch( uploadPollStart() );
+      fetch( `${ MAIN_BASE_URL }/delete/${ userType() }/${ pollId }`, {
+        method: "PUT",
+        body: data
+      } )
+        .then( response => response.json() )
+        .then( resp => {
+          dispatch( uploadPollSuccess( resp ) );
+        } )
+        .catch( err => {
+          dispatch( uploadPollFailed( err.message );)
+    });
+  }
+}
---
+++ client/src/store/reducers/reducers_poll.js
@@ -0,0 +1,191 @@
+import { 
+  CREATE_POLL_START,
+  CREATE_POLL_SUCCESS,
+  CREATE_POLL_FAILED,
+  GET_POLL_START,
+  GET_POLL_SUCCESS,
+  GET_POLL_FAILED,
+  TAG_POLL_START,
+  TAG_POLL_SUCCESS,
+  TAG_POLL_FAILED,
+  LIKE_POLL_START,
+  LIKE_POLL_SUCCESS,
+  LIKE_POLL_FAILED,
+  VOTE_POLL_START,
+  VOTE_POLL_SUCCESS,
+  VOTE_POLL_FAILED,
+  DISABLE_POLL_START,
+  DISABLE_POLL_SUCCES,
+  DISABLE_POLL_FAILED,
+  UPLOAD_POLL_PHOTO_START,
+  UPLOAD_POLL_PHOTO_SUCCESS,
+  UPLOAD_POLL_PHOTO_FAILED,
+  DELETE_POLL_START,
+  DELETE_POLL_SUCCESS,
+  DELETE_POLL_FAILED,
+} from "../actions/actions_polls";
+
+const initialState = {
+  polls: [],
+  createPollSuccess: false,
+  createPollLoading: false,
+  getPollSuccess: false,
+  getPollLoading: false,
+  tagLoading: false,
+  tagSuccess: false,
+  likeLoading: false,
+  likeSuccess: false,
+  voteLoading: false,
+  voteSuccess: false,
+  disableLoading: false,
+  disableSuccess: false,
+  uploadLoading: false,
+  uploadSuccess: false,
+  deleteLoading: false,
+  deleteSuccess: false,
+  error: {},
+}
+
+
+const pollReducers = ( state = initialState, action ) => {
+  switch ( action.type) {
+    case CREATE_POLL_START:
+      return {
+        ...state,
+        createPollLoading: true
+      }
+    case CREATE_POLL_SUCCESS:
+      return {
+        ...state,
+        createPollSuccess: true,
+        polls: state.polls.concat( action.data)
+      }
+    case CREATE_POLL_FAILED:
+      return {
+        ...state,
+        createPollLoading: false,
+        error: action.error
+      }
+    case GET_POLL_START:
+      return {
+        ...state,
+        getPollLoading: true
+      }
+    case GET_POLL_SUCCESS:
+      return {
+        ...state,
+        getPollSuccess: true,
+        polls: action.data
+      }
+    case GET_POLL_FAILED:
+      return {
+        ...state,
+        getPollLoading: false,
+        error: action.error
+      }
+    case TAG_POLL_START:
+      return {
+        ...state,
+        tagLoading: true
+      }
+    case TAG_POLL_SUCCESS:
+      return {
+        ...state,
+        tagSuccess: true,
+        polls: action.data
+      }
+    case TAG_POLL_FAILED:
+      return {
+        ...state,
+        tagLoading: false,
+        error: action.error
+      }
+    case LIKE_POLL_START:
+      return {
+        ...state,
+        likeLoading: true
+      }
+    case LIKE_POLL_SUCCESS:
+      return {
+        ...state,
+        likeSuccess: true,
+        polls: action.data
+      }
+    case LIKE_POLL_FAILED:
+      return {
+        ...state,
+        likeLoading: false,
+        error: action.error
+      }
+    case VOTE_POLL_START:
+      return {
+        ...state,
+        voteLoading: true
+      }
+    case VOTE_POLL_SUCCESS:
+      return {
+        ...state,
+        voteSuccess: true,
+        polls: action.data
+      }
+    case VOTE_POLL_FAILED:
+      return {
+        ...state,
+        voteLoading: false,
+        error: action.error
+      }
+    case DISABLE_POLL_START:
+      return {
+        ...state,
+        disableLoading: true
+      }
+    case DISABLE_POLL_SUCCES:
+      return {
+        ...state,
+        disableSuccess: true,
+        polls: action.data
+      }
+    case DISABLE_POLL_FAILED:
+      return {
+        ...state,
+        disableLoading: false,
+        error: action.error
+      }
+    case UPLOAD_POLL_PHOTO_START:
+      return {
+        ...state,
+        uploadLoading: true
+      }
+    case UPLOAD_POLL_PHOTO_SUCCESS:
+      return {
+        ...state,
+        uploadSuccess: true,
+        polls: action.data
+      }
+    case UPLOAD_POLL_PHOTO_FAILED:
+      return {
+        ...state,
+        uploadLoading: false,
+        error: action.error
+      }
+    case DELETE_POLL_START:
+      return {
+        ...state,
+        deleteLoading: true
+      }
+    case DELETE_POLL_SUCCESS:
+      return {
+        ...state,
+        deleteSuccess: true,
+        polls: action.data
+      }
+    case DELETE_POLL_FAILED:
+      return {
+        ...state,
+        deleteLoading: false,
+        error: action.error
+      }
+    default:
+      ...state;
+  }
+}
